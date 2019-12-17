618ae702e7d055eaf0f46f89c13d0d3a598eae9d
sandhawalia <hs.sandhawalia+github@gmail.com>
2019-12-17 19:08:34 +0100

    changing config params

--- dqn/doom/agent-of-doom.py
+++ dqn/doom/agent-of-doom.py
@@ -83,8 +83,9 @@ def train_agent_of_doom(config_file, device='gpu'):
         agent.reset()
         env.game.new_episode()
 
-        train_step.set_description('{0}, Reward : {1:.3f}, '
-                                   'Eps : {2:.4f}'.format(ep, total_score,
+        train_step.set_description('{0}/{1}, Reward : {2:.3f}, '
+                                   'Eps : {3:.4f}'.format(ep, step,
+                                                          total_score,
                                                           agent.eps))
 
       next_frame = env.game.get_state().screen_buffer
