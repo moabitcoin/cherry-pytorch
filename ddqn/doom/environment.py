d76613d4622cc93533cd1058c7742aa22f351f53
sandhawalia <hs.sandhawalia+github@gmail.com>
2019-12-17 22:15:03 +0100

    health gathering

--- ddqn/doom/environment.py
+++ ddqn/doom/environment.py
@@ -37,55 +37,6 @@ class DoomEnvironment():
     self.game.set_doom_scenario_path(scenario_file.as_posix())
     self.game.set_screen_resolution(ScreenResolution.RES_320X240)
     self.game.set_screen_format(ScreenFormat.GRAY8)
-
-    # Enables depth buffer.
-    self.game.set_depth_buffer_enabled(True)
-
-    # Enables labeling of in game objects labeling.
-    self.game.set_labels_buffer_enabled(True)
-
-    # Enables buffer with top down map of the current episode/level.
-    self.game.set_automap_buffer_enabled(True)
-
-    # Sets other rendering options (all of these options except
-    # crosshair are enabled (set to True) by default)
-    self.game.set_render_hud(True)
-    self.game.set_render_minimal_hud(False)  # If hud is enabled
-    self.game.set_render_crosshair(False)
-    self.game.set_render_weapon(True)
-    self.game.set_render_decals(False)  # Bullet holes and blood on the walls
-    self.game.set_render_particles(False)
-    self.game.set_render_effects_sprites(False)  # Smoke and blood
-    self.game.set_render_messages(False)  # In-game messages
-    self.game.set_render_corpses(False)
-    # Effect upon taking damage or picking up items
-    self.game.set_render_screen_flashes(True)
-
-    # Adds buttons that will be allowed.
-    self.game.add_available_button(Button.MOVE_LEFT)
-    self.game.add_available_button(Button.MOVE_RIGHT)
-    self.game.add_available_button(Button.ATTACK)
-
-    # Adds game variables that will be included in state.
-    self.game.add_available_game_variable(GameVariable.AMMO2)
-
-    # Causes episodes to finish after 200 tics (actions)
-    self.game.set_episode_timeout(200)
-
-    # Makes episodes start after 10 tics (~after raising the weapon)
-    self.game.set_episode_start_time(10)
-
-    # Makes the window appear (turned on by default)
-    self.game.set_window_visible(True)
-
-    # Turns on the sound. (turned off by default)
-    self.game.set_sound_enabled(True)
-
-    # Sets the livin reward (for each move) to -1
-    self.game.set_living_reward(-1)
-
-    # Sets ViZDoom mode (PLAYER, ASYNC_PLAYER, SPECTATOR, ASYNC_SPECTATOR,
-    # PLAYER mode is default)
     self.game.set_mode(Mode.PLAYER)
 
     self.game.init()
